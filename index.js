15331f830170f01b1a4089c04d6081738e3ab9d4
Alan <alan82222@gmail.com>
2023-09-14 14:44:39 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require ('express')
+
+const app = express()
+app.get('/',(request, response) => {
+    response.send ('hola demonio')
+})
+app.get('/',(request, response) => {
+    response.send ('hola get')
+})
+app.post('/',(request, response) => {
+    response.send ('hola post day')
+})
+app.put('/',(request, response) => {
+    response.send ('hola put*')
+})
+app.patch('/',(request, response) => {
+    response.send ('hola patch')
+})
+app.delete('/',(request, response) => {
+    response.send ('hola borrar')
+})
+
+
+/**
+ * 
+ * 
+ * 
+ *  Funcionalidad
+ */
+//http://localhost:3000
+app.listen(3000, () => {
+    console.log('listening on port 3000aaaaa')
+})
