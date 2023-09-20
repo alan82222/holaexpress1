afff933dbe0e6399b49f7e454f1ddf7cf2426d13
alan <alan82222@gmail.com>
2023-09-20 13:35:45 -0600

    modified:   request.js

--- request.js
+++ request.js
@@ -9,8 +9,25 @@ app.get('/:user/:password', (req, res)=> {
         res.json({msg: 'Inicio de sesión exitoso'})
         return
     }
-    res.json ({msg:'Error en el usuario  la contraseña' })
+    res.json ({msg:'Error en el usuario o la contraseña' })
 })
+
+// http://localhost:3000/login?user=alan&password=123
+app.get('/login', (req, res) => { //Endpoint
+    const {user, password} = req.query
+     if (!user || !password){
+        res.status(400).json({
+      msg: 'necesito la informacion de usuario y contraseña correcto'
+        })
+        return
+     }
+    if (user === 'alan' && password === '123' )  {
+        res.json({msg: 'Inicio de sesión exitoso'})
+        return
+    }
+    res.json ({msg:'Error en el usuario o la contraseña' })
+})
+
 
 app.post('/', (req, res)=> {res.json({msg:'Hello POST'})})
 app.put('/', (req, res)=> {res.json({msg:'Hello PUT'})})
